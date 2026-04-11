b166090c4f36c29bf598a88d0ab5843f843cd29b
Guilherme Lopes <guilhermelopes275@gmail.com>
2026-04-11 11:32:53 -0300

    chore: ajustar lint e build compartilhado

--- apps/backend/eslint.config.mjs
+++ apps/backend/eslint.config.mjs
@@ -1,14 +1,20 @@
 // @ts-check
 import eslint from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
+
+const compat = new FlatCompat({
+  baseDirectory: import.meta.dirname,
+  recommendedConfig: eslint.configs.recommended,
+});
 
 export default tseslint.config(
   {
     ignores: ['eslint.config.mjs'],
   },
-  eslint.configs.recommended,
+  ...compat.extends('@juscash/eslint-config'),
   ...tseslint.configs.recommendedTypeChecked,
   eslintPluginPrettierRecommended,
   {
@@ -29,7 +35,7 @@ export default tseslint.config(
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
-      "prettier/prettier": ["error", { endOfLine: "auto" }],
+      'prettier/prettier': ['error', { endOfLine: 'auto' }],
     },
   },
 );
