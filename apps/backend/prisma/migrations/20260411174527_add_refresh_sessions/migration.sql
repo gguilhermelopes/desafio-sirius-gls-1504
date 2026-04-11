3668f375d5db359f539aa9570ca2c4e202ebcdd5
Guilherme Lopes <guilhermelopes275@gmail.com>
2026-04-11 17:54:01 -0300

    chore: organiza migrations do auth

--- apps/backend/prisma/migrations/20260411174527_add_refresh_sessions/migration.sql
+++ apps/backend/prisma/migrations/20260411174527_add_refresh_sessions/migration.sql
@@ -1,16 +1,4 @@
 -- CreateTable
-CREATE TABLE "User" (
-    "id" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
-    "passwordHash" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-
-    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
 CREATE TABLE "RefreshSession" (
     "id" TEXT NOT NULL,
     "userId" TEXT NOT NULL,
@@ -24,9 +12,6 @@ CREATE TABLE "RefreshSession" (
 
     CONSTRAINT "RefreshSession_pkey" PRIMARY KEY ("id")
 );
-
--- CreateIndex
-CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
 -- CreateIndex
 CREATE INDEX "RefreshSession_userId_idx" ON "RefreshSession"("userId");
